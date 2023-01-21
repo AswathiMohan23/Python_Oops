ec127b574fa1a737b57e819fd902708c74f9f657
AswathiMohan23 <achu999here@gmail.com>
2023-01-21 16:40:54 +0530

    [ASWATHI] added . code for instance method

---
+++ Oops/Types_of_Methods/instanceMethod.py
@@ -0,0 +1,17 @@
+class Student:
+
+    school = "Abc"
+
+    def __init__(self, m1, m2, m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def avg(self):
+        return (self.m1+self.m2+self.m3)/3
+
+
+s1 = Student(34,67,20)
+s2 = Student(23,44,56)
+print(s1.avg()) # here we are passing self and not object
+print(s2.avg())
